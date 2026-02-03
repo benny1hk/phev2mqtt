b61db7460c62c625a24a256a0c70106e2e695e49
Benny <>
2026-02-03 23:59:08 +0000

    tiny fix

--- cmd/mqtt.go
+++ cmd/mqtt.go
@@ -1865,21 +1865,19 @@ func (m *mqttClient) publishHomeAssistantDiscovery(vin, topic, name string) {
 		},
 		"~": "__TOPIC__"}`,
 		// GPS tracking
-		"%s/device_tracker/%s_tracker_v2/config": `{
-			"name": "__NAME__ Tracker",
-			"unique_id": "__VIN___gps_tracker_v2",
-			"state_topic": "__TOPIC__/gps/location",
-			"json_attributes_topic": "__TOPIC__/gps/location",
-			"value_template": "{{ value_json.gps_accuracy }}",
-			"source_type": "gps",
-			"payload_available": "online",
-			"payload_not_available": "offline",
-			"availability_topic": "__TOPIC__/available",
-			"device": {
-				"identifiers": ["phev-__VIN__"],
-				"name": "PHEV __VIN__",
-				"manufacturer": "Mitsubishi"
-			}
+		"%s/device_tracker/%s_gps/config": `{
+		"name": "__NAME__ Location",
+		"unique_id": "__VIN___gps_location",
+		"state_topic": "~/gps/location",
+		"json_attributes_topic": "~/gps/location",
+		"value_template": "{{ value_json.latitude }},{{ value_json.longitude }}",
+		"source_type": "gps",
+		"device": {
+			"identifiers": ["phev-__VIN__"],
+			"name": "PHEV __VIN__",
+			"manufacturer": "Mitsubishi",
+			"model": "Outlander PHEV"
+		}
 		}`,
 		"%s/switch/%s_gps_enable/config": `{
 		"name": "__NAME__ GPS Tracking",
